dd91a6bfe869d75035c47c709893a39ef4daa656
EBIZM1 <zhang.qianqian@ebiz-fooge.com>
2018-12-12 14:08:47 +0800

    Initial Commit

---
+++ dynamicHeightSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dynamicHeightSample
+//
+//  Created by EBIZM1 on 2018/12/12.
+//  Copyright © 2018 ZhangXiQian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
